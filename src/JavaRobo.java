f1aff78f3fefa68c1baa40fb22f8b774a498529c
Mykyta <nikt615@gmail.com>
2023-06-05 13:40:55 +0300

    2

--- src/JavaRobo.java
+++ src/JavaRobo.java
@@ -1,7 +1,29 @@
 public class JavaRobo {
     public static void main(String[] args) {
-        System.out.println("Hello world");
 
-        
+        /*
+        Створити програму
+
+         Об’явити 2 змінні типу Integer та показати в консолі результати:
+
+         Додавання
+
+         Віднімання
+
+         Ділення
+
+         Множення
+         */
+        int a=6 ;
+        int b=2 ;
+
+        System.out.println("Додавання" + (a+b) );
+
+        System.out.println("Віднімання" + (a-b) );
+
+        System.out.println("Ділення" + (a/b) );
+
+        System.out.println("Множення" + (a*b) );
+
     }
 }
