83a5b0abfe7a6b8808ff826899b07dd1def63cfd
Mykyta <nikt615@gmail.com>
2023-07-06 22:42:52 +0300

    11Dz

--- src/Oop2/Passenger_car.java
+++ src/Oop2/Passenger_car.java
@@ -1,6 +1,9 @@
 package Oop2;
 
 public class Passenger_car extends Car {
+    public void Description() {
+        System.out.println("Легковий автомобіль - це транспортний засіб для перевезення пасажирів");
+    }
 
 
 }
--- src/Oop2/Truck.java
+++ src/Oop2/Truck.java
@@ -1,4 +1,7 @@
 package Oop2;
 
 public class Truck extends Car {
+    public void Description() {
+        System.out.println("Вантажний автомобіль - це транспортний засіб для перевезення вантажів");
+    }
 }
