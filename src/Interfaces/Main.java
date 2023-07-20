a15e96dab848b72ca3d95d1d9e2a811593890e7f
Mykyta <nikt615@gmail.com>
2023-07-20 13:13:51 +0300

    Dz

--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -2,16 +2,28 @@ package Interfaces;
 
 public class Main {
     public static void main(String[] args) {
-
+/*
         Driver driver2 =new Driver() ;
         Pilot pilot2 =new Pilot ();
 
         driver2.driving();
         pilot2.fly();
 
-        //Дороблю в середу и запушу
+ */
 
+       Driving[]driving1 = new  Driving []{
+               new Driver()
+       };
 
+        for (Driving driving : driving1){
+            driving.driving();
+        }
+        Fly[]fly1 = new Fly[]{
+                new Pilot()
+        };
+        for (Fly fly :fly1){
+            fly.fly();
+        }
 
 
 
