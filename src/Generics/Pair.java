550a9be9ac1048f3f1261d3b1ba11484b9b4ad8b
Mykyta <nikt615@gmail.com>
2023-07-20 16:39:58 +0300

    Dz

---
+++ src/Generics/Pair.java
@@ -0,0 +1,24 @@
+package Generics;
+
+public class Pair <A,B>{
+
+
+    private A first;
+    private B second;
+
+    public A getFirst() {
+        return first;
+    }
+
+    public void setFirst(A first) {
+        this.first = first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    public void setSecond(B second) {
+        this.second = second;
+    }
+}
---
+++ src/Generics/sting.java
@@ -0,0 +1,9 @@
+package Generics;
+
+import java.sql.SQLOutput;
+
+public class sting {
+    public static void main(String[] args)
+Pair pair1 = new Pair();
+    pair1.setFirst();
+}
