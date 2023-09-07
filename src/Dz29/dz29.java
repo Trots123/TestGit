bba973d04e897d0a1541ae35b4354d8711ac78fc
Mykyta <nikt615@gmail.com>
2023-09-07 19:44:52 +0300

    Dz29

--- src/Dz29/dz29.java
+++ src/Dz29/dz29.java
@@ -5,13 +5,13 @@ public class dz29 {
 
         Thread thread1 = new Thread(() -> {
             for(int i=0; i<=10; i++){
-                System.out.println("Thread #1. First thread iteration #" + i);
+                System.out.println("Thread #1.#" + i);
             }
         });
 
         Thread thread2 = new Thread(() -> {
             for(int j=0; j<=10; j++){
-                System.out.println("Thread #2. Second thread iteration #" + j);
+                System.out.println("Thread #2.#" + j);
             }
         });
 
